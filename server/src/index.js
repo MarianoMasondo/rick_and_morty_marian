c00eb18b4f1b21f5fd93157d559d3d276c6e618f
Mariano Masondo <nanomason@gmail.com>
2023-10-18 17:03:40 -0300

    Deploy

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("./routes/index.js");
 const server = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
