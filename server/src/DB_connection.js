ecc63a94eb0945110019019f282aaf50f50a61bc
Mariano Masondo <nanomason@gmail.com>
2023-12-09 19:22:46 -0300

    Deploy cancelled

--- server/src/DB_connection.js
+++ server/src/DB_connection.js
@@ -1,34 +1,35 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY, DB_PORT, DB_NAME, DB_DIALECT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 const FavoriteModel = require("./models/Favorite.js");
-const UserModel = require("./models/User.js");
+const FavoriteModel = require("./models/User.js");
 
+// EJERCICIO 03
+// A la instancia de Sequelize le falta la URL de conexión. ¡Agrégala!
+// Recuerda pasarle la información de tu archivo '.env'.
+
+// URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
 const sequelize = new Sequelize(
-  `${DB_DIALECT}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  { logging: false },
-  {
-    dialectOptions: {
-      ssl: {
-        require: true
-      }
-    }
-  }
+  // URL
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+  { logging: false, native: false }
 );
+// const sequelize = new Sequelize(DB_DEPLOY,
+//   { logging: false, native: false }
+// );
 
+// EJERCICIO 05
+// Debajo de este comentario puedes ejecutar la función de los modelos.
 FavoriteModel(sequelize);
 UserModel(sequelize);
-
-const { User, Favorite } = sequelize.models;
-
 // Ejercicio 06
 // ¡Relaciona tus modelos aquí abajo!
-User.belongsToMany(Favorite, { through: "user_favorite" });
-Favorite.belongsToMany(User, { through: "user_favorite" });
+const { User, Favorite } = sequelize.models;
+User.belongsToMany(Favorite, { throw: "user_favorite" });
+Favorite.belongsToMany(User, { throw: "user_favorite" });
 
 module.exports = {
   User,
   Favorite,
   conn: sequelize,
 };
-
