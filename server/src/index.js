1765054b05cfca309db386a10b9f172099c68362
Mariano Masondo <nanomason@gmail.com>
2023-12-12 16:54:36 -0300

    Front deployed

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes/index.js");
 const server = express();
 const PORT = 3001;
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(cors({ origin: "https://rick-and-morty-marian.vercel.app" }));
 server.use(express.json());
 
 server.use("/rickandmorty", router);
@@ -20,3 +12,4 @@ server.use("/rickandmorty", router);
 server.listen(PORT, () => {
   console.log("Server raised in port: " + PORT);
 });
+
