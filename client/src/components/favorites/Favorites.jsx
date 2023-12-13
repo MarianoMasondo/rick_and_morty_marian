efaff7b16fe8355bf542d90daa12d06db5145a80
Mariano Masondo <nanomason@gmail.com>
2023-12-13 13:09:15 -0300

    sin cambios importantes

--- client/src/components/favorites/Favorites.jsx
+++ client/src/components/favorites/Favorites.jsx
@@ -1,71 +1,47 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import Card from '../card/Card';
-// import styles from './Favorites.module.css';
-// import { removeFav } from '../../redux/actions';
+import React from 'react';
+import { connect } from 'react-redux';
+import Card from '../card/Card';
+import styles from './Favorites.module.css';
+import { removeFav } from '../../redux/actions';
 
-// function Favorites({ myFavorites, removeFav }) {
-//   const onClose = (id, removeFromHome) => {
-//     removeFav(id);
-//     if (removeFromHome) {
-//       console.log(`Removing from home: ${id}`);
-//     }
-//   };
+function Favorites({ myFavorites, removeFav }) {
+  const onClose = (id, removeFromHome) => {
+    removeFav(id);
+    if (removeFromHome) {
+      console.log(`Removing from home: ${id}`);
+    }
+  };
 
-//   return (
-//     <div className={styles.favContainer}>
-//       {myFavorites.map((character, index) => (
-//         <Card
-//           key={index}
-//           id={character.id}
-//           name={character.name}
-//           status={character.status}
-//           species={character.species}
-//           gender={character.gender}
-//           origin={character.origin}
-//           image={character.image}
-//           onClose={() => onClose(character.id, false)}        
-//         />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export function mapStateToProps(state) {
-//   return {
-//     myFavorites: state.myFavorites,
-//   };
-// }
-
-// export function mapDispatchToProps(dispatch) {
-//   return {
-//     removeFav: (id) => dispatch(removeFav(id)),
-//   };
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
-// Favorites.jsx
-
-// Favorites.jsx
-
-// Favorites.jsx
-
-
-import Card from "../card/Card";
-
-const Favorites = ({ characters, onClose }) => {
   return (
-    <div>
-      <h2>Favorites</h2>
-      {characters.map((character) => (
-        <Card key={character.id} character={character} onClose={onClose} />
+    <div className={styles.favContainer}>
+      {myFavorites.map((character, index) => (
+        <Card
+          key={index}
+          id={character.id}
+          name={character.name}
+          status={character.status}
+          species={character.species}
+          gender={character.gender}
+          origin={character.origin}
+          image={character.image}
+          onClose={() => onClose(character.id, false)}        
+        />
       ))}
     </div>
   );
-};
+}
 
-export default Favorites;
+export function mapStateToProps(state) {
+  return {
+    myFavorites: state.myFavorites,
+  };
+}
 
+export function mapDispatchToProps(dispatch) {
+  return {
+    removeFav: (id) => dispatch(removeFav(id)),
+  };
+}
 
-
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
 
