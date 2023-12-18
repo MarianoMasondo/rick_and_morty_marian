339198d81d98f99c328c0a141cc8cdcc151a423f
Mariano Masondo <nanomason@gmail.com>
2023-12-17 21:34:31 -0300

    Local Storage

--- client/src/App.js
+++ client/src/App.js
@@ -116,7 +116,7 @@ function App() {
                   randomCharacter={generarRandomId}
                   logout={logout}
                 />
-                <Favorites onClose={onClose} showCloseButton={false}/>
+                <Favorites characters={characters} onClose={onClose} showCloseButton={false}/>
               </div>
             }
           />
