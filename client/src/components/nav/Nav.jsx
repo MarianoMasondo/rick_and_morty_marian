14bb9c44c60c2cc5b00d34c4fadac72e4527c79d
Mariano Masondo <nanomason@gmail.com>
2026-05-01 12:20:24 -0300

    Fix loguot button

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -2,39 +2,34 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./Nav.module.css";
 import SearchBar from "../searchBar/SearchBar.jsx";
-import { logout } from "../../redux/actions.js";
-import { useDispatch } from "react-redux";
 
 export default function Nav(props) {
-  const dispatch = useDispatch();
-
-  const handleLogout = () => {
-    dispatch(logout());
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <NavLink to="/home">
           <button>Home</button>
         </NavLink>
+
         <NavLink to="/favorites">
           <button>Favorites</button>
         </NavLink>
+
         <NavLink to="/home">
           <button onClick={props.randomCharacter}>Random</button>
         </NavLink>
       </div>
+
       <div className={styles.center}>
         <SearchBar onSearch={props.onSearch} />
       </div>
+
       <div className={styles.right}>
         <NavLink to="/about">
           <button>About</button>
         </NavLink>
-        <NavLink to="/" onClick={handleLogout}>
-          <button>Logout</button>
-        </NavLink>
+
+        <button onClick={props.logout}>Logout</button>
       </div>
     </div>
   );
